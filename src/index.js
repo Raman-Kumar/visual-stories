60347853d43ef80caa3f267dc1b34672483798ff
Raman kumar <k.raman1998@yahoo.in>
2022-09-08 17:33:03 +0530

    set up react files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM  from "react-dom";
+import React from 'react'
+import App from "./App";
+import './index.css'
+
+ReactDOM.render(
+    <React.StrictMode >
+       <App />
+    </React.StrictMode>, 
+    document.getElementById('root')
+)
+
+
+// import ReactDOM from 'react-dom'
+
+// import App from './App.js'
+// import './index.css'
+
+// ReactDOM.render(
+//     <React.StrictMode >
+//         <App />
+//     </React.StrictMode>, 
+//     document.getElementById('root')
+// )
