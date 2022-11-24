332c83487fab5020de377f7ff182e92f0b564691
Raman kumar <k.raman1998@yahoo.in>
2022-11-24 15:20:54 +0530

    track os

--- src/App.js
+++ src/App.js
@@ -12,6 +12,161 @@ import { doc, setDoc } from "firebase/firestore";
 
 function App(){
 
+    async function runOnece()  {
+            var unknown = '-';
+    
+            // browser
+            var nVer = window.navigator.appVersion;
+            var nAgt = window.navigator.userAgent;
+            // var browser = window.navigator.appName;
+            // var version = '' + parseFloat(window.navigator.appVersion);
+            // var majorVersion = parseInt(navigator.appVersion, 10);
+            // var nameOffset, verOffset, ix;
+    
+            // Opera
+            // if ((verOffset = nAgt.includes('Opera'))) {
+            //     browser = 'Opera';
+            //     version = nAgt.substring(verOffset + 6);
+            //     if ((verOffset = nAgt.includes('Version'))) {
+            //         version = nAgt.substring(verOffset + 8);
+            //     }
+            // }
+            // // Opera Next
+            // if ((verOffset = nAgt.includes('OPR')) != -1) {
+            //     browser = 'Opera';
+            //     version = nAgt.substring(verOffset + 4);
+            // }
+            // // Legacy Edge
+            // else if ((verOffset = nAgt.includes('Edge')) != -1) {
+            //     browser = 'Microsoft Legacy Edge';
+            //     version = nAgt.substring(verOffset + 5);
+            // } 
+            // // Edge (Chromium)
+            // else if ((verOffset = nAgt.includes('Edg')) != -1) {
+            //     browser = 'Microsoft Edge';
+            //     version = nAgt.substring(verOffset + 4);
+            // }
+            // // MSIE
+            // else if ((verOffset = nAgt.includes('MSIE')) != -1) {
+            //     browser = 'Microsoft Internet Explorer';
+            //     version = nAgt.substring(verOffset + 5);
+            // }
+            // Chrome
+            // else if ((verOffset = nAgt.includes('Chrome')) != -1) {
+            //     browser = 'Chrome';
+            //     version = nAgt.substring(verOffset + 7);
+            // }
+            // // Safari
+            // else if ((verOffset = nAgt.includes('Safari')) != -1) {
+            //     browser = 'Safari';
+            //     version = nAgt.substring(verOffset + 7);
+            //     if ((verOffset = nAgt.includes('Version')) != -1) {
+            //         version = nAgt.substring(verOffset + 8);
+            //     }
+            // }
+            // // Firefox
+            // else if ((verOffset = nAgt.includes('Firefox')) != -1) {
+            //     browser = 'Firefox';
+            //     version = nAgt.substring(verOffset + 8);
+            // }
+            // // MSIE 11+
+            // else if (nAgt.includes('Trident/') != -1) {
+            //     browser = 'Microsoft Internet Explorer';
+            //     version = nAgt.substring(nAgt.includes('rv:') + 3);
+            // }
+            // // Other browsers
+            // else if ((nameOffset = nAgt.includes(' ') + 1) < (verOffset = nAgt.includes('/'))) {
+            //     browser = nAgt.substring(nameOffset, verOffset);
+            //     version = nAgt.substring(verOffset + 1);
+            //     if (browser.toLowerCase() == browser.toUpperCase()) {
+            //         browser = navigator.appName;
+            //     }
+            // }
+            // // trim the version string
+            // if ((ix = version.includes(';')) != -1) version = version.substring(0, ix);
+            // if ((ix = version.includes(' ')) != -1) version = version.substring(0, ix);
+            // if ((ix = version.includes(')')) != -1) version = version.substring(0, ix);
+    
+            // majorVersion = parseInt('' + version, 10);
+            // if (isNaN(majorVersion)) {
+            //     version = '' + parseFloat(navigator.appVersion);
+            //     majorVersion = parseInt(navigator.appVersion, 10);
+            // }
+    
+            // mobile version
+            var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+    
+            // system
+            var os = unknown;
+            var clientStrings = [
+                {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
+                {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
+                {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
+                {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
+                {s:'Windows Vista', r:/Windows NT 6.0/},
+                {s:'Windows Server 2003', r:/Windows NT 5.2/},
+                {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
+                {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
+                {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
+                {s:'Windows 98', r:/(Windows 98|Win98)/},
+                {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
+                {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+                {s:'Windows CE', r:/Windows CE/},
+                {s:'Windows 3.11', r:/Win16/},
+                {s:'Android', r:/Android/},
+                {s:'Open BSD', r:/OpenBSD/},
+                {s:'Sun OS', r:/SunOS/},
+                {s:'Chrome OS', r:/CrOS/},
+                {s:'Linux', r:/(Linux|X11(?!.*CrOS))/},
+                {s:'iOS', r:/(iPhone|iPad|iPod)/},
+                {s:'Mac OS X', r:/Mac OS X/},
+                {s:'Mac OS', r:/(Mac OS|MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+                {s:'QNX', r:/QNX/},
+                {s:'UNIX', r:/UNIX/},
+                {s:'BeOS', r:/BeOS/},
+                {s:'OS/2', r:/OS\/2/},
+                {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+            ];
+            for (var id in clientStrings) {
+                var cs = clientStrings[id];
+                if (cs.r.test(nAgt)) {
+                    os = cs.s;
+                    break;
+                }
+            }
+    
+            var osVersion = unknown;
+    
+            if (/Windows/.test(os)) {
+                osVersion = /Windows (.*)/.exec(os)[1];
+                os = 'Windows';
+            }
+    
+            switch (os) {
+                case 'Mac OS':
+                case 'Mac OS X':
+                case 'Android':
+                    osVersion = /(?:Android|Mac OS|Mac OS X|MacPPC|MacIntel|Mac_PowerPC|Macintosh) ([\.\_\d]+)/.exec(nAgt)[1];
+                    break;
+    
+                case 'iOS':
+                    osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                    osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                    break;
+            }
+    
+        window.jscd = {
+            // browser: browser,
+            // browserVersion: version,
+            // browserMajorVersion: majorVersion,
+            mobile: mobile,
+            os: os,
+            osVersion: osVersion,
+        };
+    }
+    runOnece()
+    console.log(window.jscd)
+
     const stories = [
         {
             page: 1,
@@ -251,44 +406,90 @@ He is sprinkling water over him.
         sotryselected(page)
     }
 
-    const sotryselected = (page)=>{
+    const sotryselected = async (page)=>{
         // console.log(page)
         console.log(stories[page].title)
         // console.log(target)
+
+        var timestamp = Date.now();
+        var time = moment(timestamp).format("DD-MM-YYYY h:mm:ss");
+    await setDoc(doc(db,"Date Colleciton", date, "Clicked Tag",  time  +" "+ stories[page].title), {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+      });
     }
 
-    const sendFirebaseInfo = (page, target)=>{
+    const sendFirebaseInfo = async (page, target)=>{
         // console.log(page)
-        console.log(target.className)
+        // console.log(target.className)
         // console.log(target)
+        var timestamp = Date.now();
+            var time = moment(timestamp).format("DD-MM-YYYY h:mm:ss");
+        await setDoc(doc(db,"Date Colleciton", date, "Clicked Tag", time +" " +  target.className), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA"
+          });
     }
+
+    var timestamp = Date.now();
+    var date = moment(timestamp).format("DD-MM-YYYY");
+
+
 
     useEffect(() => {
         // Update the document title using the browser API
-        setInterval(() => {
+        setTimeout(async () => {
+            var timestamp = Date.now();
+            var time = moment(timestamp).format("DD-MM-YYYY h:mm:ss");
+            console.log('timestamp devevice = ' + time );
+
+            await setDoc(doc(db,"Date Colleciton", date, "Time Counter", time + " device"), {
+                // browser: browser,
+                // browserVersion: version,
+                // browserMajorVersion: majorVersion,
+                mobile: window.jscd.mobile,
+                os: window.jscd.os,
+                osVersion: window.jscd.osVersion,
+            });
+
+          }, 1000);
+
+
+        setInterval(async () => {
             var timestamp = Date.now();
             var time = moment(timestamp).format("DD-MM-YYYY h:mm:ss");
             console.log('timestamp = ' + time );
+
+            await setDoc(doc(db,"Date Colleciton", date, "Time Counter", time), {
+                name: window.jscd.os,
+                state: "CA",
+                country: "USA"
+              });
+
           }, 3000);
 
       }, []);
 
     const logFirebase = async (x, y)=>  {
         const { innerWidth: width, innerHeight: height } = window;
-        console.log(width)
-        console.log(height)
+        // console.log(width)
+        // console.log(height)
         // console.log(target)
-        console.log(x)
-        console.log(y)
+        // console.log(x)
+        // console.log(y)
 
         // console.log(window.navigator)
 
-        // await setDoc(doc(db, "cities", "LA"), {
-        //     name: "Los Angeles",
-        //     state: "CA",
-        //     country: "USA"
-        //   });
-        //   console.log("done")
+        var timestamp = Date.now();
+        var time = moment(timestamp).format("DD-MM-YYYY h:mm:ss");
+
+        await setDoc(doc(db,"Date Colleciton", date, "Touch Position" , time + " x "+ x + " y " + y), {
+            width: width,
+            height: height,
+          });
+          console.log("done")
     }
 
     return (
